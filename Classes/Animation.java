0149f08d0d82b4a1dd02ddc920de6a1ac5ceed4b
Pedro Amaral <amaral1@ufpr.br>
2025-01-07 22:12:05 -0300

    Create Input and Animation classes

---
+++ Classes/Animation.java
@@ -0,0 +1,31 @@
+package Classes;
+
+public class Animation implements Runnable 
+{
+    private volatile boolean running = true;
+
+    @Override
+    public void run() 
+    {
+        // Animation login
+        while (running) 
+        {
+            try 
+            {
+                // Animation speed
+                Thread.sleep(500);
+
+                // Animation itself -- HERE
+            } 
+            catch (InterruptedException e)
+            {
+                running = false;
+            }
+        }
+    }
+
+    public void stop() 
+    {
+        running = false;
+    }
+}
---
+++ Classes/KeyInputListener.java
@@ -0,0 +1,39 @@
+package Classes;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import javax.swing.JFrame;
+
+public class KeyInputListener extends JFrame implements KeyListener {
+    private volatile char input = ' ';
+
+    public KeyInputListener() {
+        setTitle("KeyInputListener");
+        setSize(400, 400); // Ajuste o tamanho conforme necessário
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        addKeyListener(this);
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        // Não faz nada na borda de subida
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        input = e.getKeyChar();
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        // Não faz nada
+    }
+
+    public char getInput() {
+        return input;
+    }
+
+    public void resetInput() {
+        input = ' ';
+    }
+}
